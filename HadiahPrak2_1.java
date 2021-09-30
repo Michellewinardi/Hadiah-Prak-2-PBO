eba38a8025395f7e6c490a1d51ba9cf098cc6fc9
Michellewinardi <90232337+Michellewinardi@users.noreply.github.com>
2021-09-30 12:53:48 +0800

    Add files via upload

---
+++ HadiahPrak2_1.java
@@ -0,0 +1,21 @@
+// Soal Genap Nomor 1
+// NIM : 51019016
+
+public class HadiahPrak2_1 {
+	
+	public static void main(String[] args) {
+	
+		float panjang,lebar,luas;
+	
+		panjang=2;
+		lebar=5;
+	
+		System.out.println("Panjang Persegi Panjang : " +panjang);
+		System.out.println("Lebar Persegi Panjang : " +lebar);
+		System.out.println("\n");
+		luas=panjang*lebar;
+		System.out.println("Hasil Luas Persegi Panjang : " + luas);
+	
+
+	}
+}
---
+++ HadiahPrak2_2.java
@@ -0,0 +1,33 @@
+// Soal Genap Nomor 2
+// NIM : 51019016
+
+import java.util.Scanner;
+public class HadiahPrak2_2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+	
+		int jam,menit,detik,totaldetik;
+	
+		System.out.println("Ini Soal Genap Nomor 2");
+	
+		System.out.print("Masukkan Jam : ");
+		jam = input.nextInt();
+	
+		System.out.print("Masukkan Menit : ");
+		menit = input.nextInt();
+	 
+		System.out.print("Masukkan Detik : ");
+		detik = input.nextInt();
+	
+		System.out.println("\n");
+	
+		totaldetik=(jam*3600)+(menit*60)+detik;
+	
+		System.out.println("Total Detik : " +totaldetik);
+	
+
+	}
+
+}
